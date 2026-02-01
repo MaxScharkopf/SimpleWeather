7e10da65ebcf4cf37d3119deb46b639723995b0b
Maxwell Scharkopf <maxwell.scharkopf@gmail.com>
2026-02-01 14:05:56 -0800

    Initial Commit

---
+++ SimpleWeather/SimpleWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherApp.swift
+//  SimpleWeather
+//
+//  Created by Maxwell Scharkopf on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
